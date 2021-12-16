8bc4cc3daec6885d88eda5b50e7cc2d3231df3a4
alex <arodriguez202020@gmail.com>
2021-12-16 12:15:30 -0600

    create state for list and items and set to empty string and array,  create hanldchange function and addItem funtion to handle new text and add text value to items array but still display previous array items

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setList(value);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, list];
+    });
+    setList("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={list} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
