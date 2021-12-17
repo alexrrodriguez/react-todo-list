57b3491458686f92c91aff94e8fd27310e70d4e1
alex <arodriguez202020@gmail.com>
2021-12-17 17:26:51 -0600

    create InputArea componentthat passes in props and returns div with input and button, add onclick to button that return empty function that return props.onAdd with input Text value passed, in App.jsx add onAdd prop to InputArea component that calls additem function iwth inputText passed through which renders serItems state function with prevItems function that return new array with input text value appended

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const value = event.target.value;
-    setInputText(value);
-  }
-
-  function addItem() {
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
-    setInputText("");
   }
 
   function deleteItem(id) {
@@ -31,10 +25,7 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
+        <InputArea onAdd={addItem} />
       </div>
       <div>
         <ul>
